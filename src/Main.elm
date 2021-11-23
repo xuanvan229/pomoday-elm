5737c5bf277b722d144f1b01ecc58e4b92d253ef
Hong Xuan <xuanvan229@gmail.com>
2021-11-23 18:10:11 +0700

    add modal input

--- src/Main.elm
+++ src/Main.elm
@@ -2,11 +2,13 @@ module Main exposing (..)
 
 import Browser
 import Browser.Navigation as Nav
+import Browser.Dom as Dom
 import Browser.Events as Events
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
 import Json.Decode as Decode
+import Task
 
 
 
@@ -33,12 +35,14 @@ type alias Model =
   { key : Nav.Key
   , url : Url.Url
   , char : Char
+  , text: String
+  , show : Bool
   }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( Model key url 'C', Cmd.none )
+  ( Model key url 'C' "" False, Cmd.none )
 
 
 
@@ -50,11 +54,14 @@ type Msg
   | UrlChanged Url.Url
   | PressedLetter Char
   | PressedString String
+  | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
+    NoOp ->
+      ( model, Cmd.none )
     LinkClicked urlRequest ->
       case urlRequest of
         Browser.Internal url ->
@@ -68,11 +75,18 @@ update msg model =
       , Cmd.none
       )
     PressedLetter letter ->
-      ( { model | char = letter }, Cmd.none )
+      ( { model | char = letter, show= True }, focusSearchBox )
     PressedString string ->
-      ( model, Cmd.none )
+      if string == "Escape" then
+        ( { model | show = False }, Cmd.none )
+      else
+        ( { model | text = string, show = True }, Cmd.none )
 
 
+
+focusSearchBox : Cmd Msg
+focusSearchBox =
+    Task.attempt (\_ -> NoOp) (Dom.focus "input-box")
 
 -- SUBSCRIPTIONS
 
@@ -98,14 +112,31 @@ toKey string =
 -- VIEW
 
 
+renderInputModal : Model -> Html Msg
+renderInputModal model =
+  if model.show
+    then
+      div [
+        class "fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center"
+      ] [
+        div [class "w-9/12 p-2 bg-white rounded-md shadow-lg bg-gray-100 border-indigo-400	"] [
+          input [id "input-box", class "w-full text-sm bg-transparent p-2 outline-none", placeholder "Type anything here...", autofocus True] []
+        ]
+      ]
+    else
+      div [] []
+
 view : Model -> Browser.Document Msg
 view model =
   { title = "Pomoday Elm"
   , body =
   [
-    div [class "flex items-center text-2xl mt-10"] [
+    div [class "flex font-mono items-center text-2xl mt-10"] [
+      renderInputModal(model),
       h1 [ class "font-bold mr-4" ] [text "Pomoday elm: "] ,
-      b [ ] [ text (String.fromChar model.char) ]
+      b [ ] [ text (String.fromChar model.char) ],
+      br [] [],
+      b [ ] [ text (model.text) ]
     ]
   ]
   }
